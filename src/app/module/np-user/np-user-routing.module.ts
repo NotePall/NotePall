872cf6c3155dd0877376220ad4e339d133b29847
mofirojean <mofirojean@gmail.com>
2023-05-25 12:49:40 -0700

    feat: This handles the navigations in the users page

--- src/app/module/np-user/np-user-routing.module.ts
+++ src/app/module/np-user/np-user-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NpUserComponent } from './np-user.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { NpUserComponent } from './components/dashboard/np-user.component';
+import { FavoriteComponent } from './components/favorite/favorite.component';
+import { NoteComponent } from './components/note/note.component';
+import { StickynoteComponent } from './components/stickynote/stickynote.component';
 
-const routes: Routes = [{ path: '', component: NpUserComponent }];
+const routes: Routes = [{ 
+  path: '', component: NpUserComponent,
+  children: [
+    { component: NoteComponent, path: "note" },
+    { component: StickynoteComponent, path: "stickynote" },
+    { component: CategoriesComponent, path: "categories" },
+    { component: FavoriteComponent, path: "favorite" }
+  ]
+ }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/module/np-user/np-user.module.ts
+++ src/app/module/np-user/np-user.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { NpUserRoutingModule } from './np-user-routing.module';
 import { NpUserComponent } from './components/dashboard/np-user.component';
+import { NoteComponent } from './components/note/note.component';
+import { StickynoteComponent } from './components/stickynote/stickynote.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { FavoriteComponent } from './components/favorite/favorite.component';
 
 
 @NgModule({
   declarations: [
-    NpUserComponent
+    NpUserComponent,
+    NoteComponent,
+    StickynoteComponent,
+    CategoriesComponent,
+    FavoriteComponent
   ],
   imports: [
     CommonModule,

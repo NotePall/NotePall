7041cc62f2f8a201a611f008eb507f703ce330d2
mofirojean <mofirojean@gmail.com>
2023-05-25 12:42:07 -0700

    feat: created the signup page

---
+++ src/app/module/np-auth/components/signup/signup.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  gotToSigInPage = () => {
+    this.router.navigate(['/auth/login'])
+  }
+
+}

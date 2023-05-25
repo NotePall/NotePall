15b9769ab06a195677a97405cc3d7cb411e1cf89
mofirojean <mofirojean@gmail.com>
2023-05-25 12:39:04 -0700

    feat: created the landing page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [{ path: 'auth', loadChildren: () => import('./module/np-auth/np-auth.module').then(m => m.NpAuthModule) }, { path: 'user', loadChildren: () => import('./module/np-user/np-user.module').then(m => m.NpUserModule) }];
+const routes: Routes = [
+  { path: 'home', component: LandingPageComponent },
+  { path: 'auth', loadChildren: () => import('./module/np-auth/np-auth.module').then(m => m.NpAuthModule) }, 
+  { path: 'user', loadChildren: () => import('./module/np-user/np-user.module').then(m => m.NpUserModule) },
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

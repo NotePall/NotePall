b022709ccded8dbcc13abd279517a5afadcc4cae
mofirojean <mofirojean@gmail.com>
2023-05-25 12:48:38 -0700

    feat: created the sticky note component

---
+++ src/app/module/np-user/components/stickynote/stickynote.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stickynote',
+  templateUrl: './stickynote.component.html',
+  styleUrls: ['./stickynote.component.scss']
+})
+export class StickynoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

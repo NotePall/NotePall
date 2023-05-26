15bd02faf1d1d9b1fcb9a39080e073da5ad4963e
mofirojean <mofirojean@gmail.com>
2023-05-26 16:21:15 -0700

    feat: added new styles for the app sticky note

--- src/app/module/np-user/components/stickynote/stickynote.component.ts
+++ src/app/module/np-user/components/stickynote/stickynote.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Color } from 'src/app/models/color.model';
+import { Label } from 'src/app/models/label.model';
+import { StickyNote } from 'src/app/models/stikynote.model';
 
 @Component({
   selector: 'app-stickynote',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StickynoteComponent implements OnInit {
 
+  colors!: Color[];
+  selectedColor!: string;
+
+  labels!: Label[];
+  selectedLabel!: string;
+
+  stickNote!: StickyNote[]
+
   constructor() { }
 
   ngOnInit(): void {
+    this.colors = [
+      {name: 'orange', color: "rgba-color"},
+      {name: 'blue', color: "rgba-color"},
+      {name: 'green', color: "rgba-color"},
+      {name: 'grey', color: "rgba-color"},
+      {name: 'yellow', color: "rgba-color"},
+    ]
+
+    this.labels = [
+      {name: "fun", label: "fun"},
+      {name: "happy", label: "happy"},
+      {name: "sad", label: "sad"},
+      {name: "love", label: "love"},
+    ]
   }
 
 }

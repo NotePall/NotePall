22405d7b7543dc05c27de4dd99d2e907d100663d
mofirojean <mofirojean@gmail.com>
2023-05-26 16:13:23 -0700

    chore: added methods of login to frontend

--- src/app/module/np-auth/components/signin/signin.component.ts
+++ src/app/module/np-auth/components/signin/signin.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { UserRequest } from 'src/app/models/user.model';
+import { AuthService } from 'src/app/service/auth.service';
+import { LocalStorageUtils } from 'src/app/utils/localstorage.utils';
+import { addToMessageService } from 'src/app/utils/message.utils';
 
 @Component({
   selector: 'app-signin',
@@ -8,12 +14,46 @@ import { Router } from '@angular/router';
 })
 export class SigninComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  loginFormData!: FormGroup
+
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private messageService: MessageService
+  ) { }
 
   ngOnInit(): void {
+    this.loginFormData = this.fb.group({
+      username: new FormControl(''),
+      password: new FormControl('')
+    })
   }
 
   gotToSignUpPage = () => {
     this.router.navigate(['/auth/signup'])
+  }
+
+  login = () => {
+    const loginForm: UserRequest = {
+      username: this.loginFormData.get(['username'])?.value,
+      password: this.loginFormData.get(['password'])?.value,
+    }
+
+    console.log(loginForm)
+
+    this.authService.login(loginForm).subscribe({
+      next: res => {
+        LocalStorageUtils.deleteToken()
+        LocalStorageUtils.writeToken(res.token);
+        addToMessageService(this.messageService, 'success', 'Success', `Successfull Login`)
+        this.router.navigate(['/user/stickynote'])
+      },
+      error: err => {
+        console.log(err);
+        addToMessageService(this.messageService, 'error', 'Error', `Wrong Credentials`)
+      }
+    })
+
   }
 }
--- src/app/module/np-auth/components/signup/signup.component.ts
+++ src/app/module/np-auth/components/signup/signup.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-
+import { MessageService } from 'primeng/api';
+import { User } from 'src/app/models/user.model';
+import { AuthService } from 'src/app/service/auth.service';
+import { addToMessageService } from 'src/app/utils/message.utils';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -8,13 +12,46 @@ import { Router } from '@angular/router';
 })
 export class SignupComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  registerForm!: FormGroup;
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private fb: FormBuilder,
+    private messageService: MessageService
+  ) { }
 
   ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      username: new FormControl(''),
+      email: new FormControl(''),
+      password: new FormControl(''),
+      confirm_password: new FormControl('')
+    })
   }
 
   gotToSigInPage = () => {
     this.router.navigate(['/auth/login'])
+  }
+
+  registerUser = () => {
+    const userData: User = {
+      username: this.registerForm.get(['username'])?.value,
+      email: this.registerForm.get(['email'])?.value,
+      password: this.registerForm.get(['password'])?.value
+    }
+
+    console.log(userData)
+
+    this.authService.register(userData).subscribe({
+      next: res => {
+        // call the message service
+        addToMessageService(this.messageService, 'success', 'Success', `Registration Successfull`)
+      },
+      error: err => {
+        //  report error to user
+        addToMessageService(this.messageService, 'warn', 'Error', `Unable To Create An Account\n Check your Cridentials`)
+      }
+    })
   }
 
 }
